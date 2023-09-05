86447c20b4da3dd261b1e7ae6d628502a6539602
Lorenzo La Farciola <lorenzo.lafarciola@studio.unibo.it>
2023-09-05 18:06:42 +0200

    Changed files

--- LorenzoLaFarciola/LorenzoLaFarciolaTest/CellsImplTest.cs
+++ LorenzoLaFarciola/LorenzoLaFarciolaTest/CellsImplTest.cs
@@ -1,13 +1,15 @@
 ﻿using LorenzoLaFarciola;
+using NUnit.Framework;
 
 namespace LorenzoLaFarciolaTest;
 
+[TestFixture]
 public class CellsImplTest
 {
-    
+    [Test]
     public void TestCellsImpl()
     {
         CellsImpl cells = new CellsImpl(TroopType.Axe, true);
-        //Assert.IsTrue(new CellsImpl(TroopType.Axe, true).Equals(cells));
+        Assert.IsTrue(new CellsImpl(TroopType.Axe, true).Equals(cells));
     }
 }
