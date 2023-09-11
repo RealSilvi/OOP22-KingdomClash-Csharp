95e51be72df96f420b79aa2d9b97960c0257a706
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-09-11 15:29:40 +0200

    add the classes

---
+++ AbdoulayeKane/AbdoulayeKane/CityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿
+namespace AbdoulayeKane;
+
+public class CityConfiguration
+{
+   private readonly int DefaultRows = 5;
+   private readonly int DefaultColums = 5;
+
+   private readonly int width;
+   private readonly int height;
+   
+
+   public CityConfiguration()
+   {
+      width = DefaultColums;
+      height = DefaultRows;
+
+   }
+
+   public int GetHeight()
+   {
+      return width;
+   }
+
+   public int GetWidth()
+   {
+      return height;
+   }
+}
---
+++ AbdoulayeKane/AbdoulayeKane/PathIconsConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace AbdoulayeKane;
+
+public class PathIconsConfiguration
+{
+    public enum BuildingTypes
+    {
+        FARM,
+        LUMBERJACK,
+        HALL
+        
+    }
+
+    private readonly string directoryPath = "OOP22-KingdomClash-Csharp\\AbdoulayeKane\\city\\buildings";
+    private Dictionary<BuildingTypes, Dictionary<int, String>> buildings;
+
+    public PathIconsConfiguration() {
+        buildings = new Dictionary<BuildingTypes, Dictionary<int, string>>()
+        {
+            {
+                BuildingTypes.FARM, new Dictionary<int, String>()
+                {
+                    { 1, directoryPath + "farm1.png" },
+                    { 2, directoryPath + "farm2.png" },
+                    { 3, directoryPath + "farm3.png" }
+                }
+            },
+            {
+                BuildingTypes.HALL, new Dictionary<int, string>()
+                {
+                    { 1, directoryPath + "hall1.png" },
+                    { 2, directoryPath + "hall2.png" },
+                    { 3, directoryPath + "hall3.png" } 
+                }
+            },
+            {
+                BuildingTypes.LUMBERJACK, new Dictionary<int, string>()
+                {
+                    { 1, directoryPath + "lumberjack1.png" },
+                    { 2, directoryPath + "lumberjack2.png" },
+                    { 3, directoryPath + "lumberjack3.png" } 
+                }
+            }
+
+        };
+
+    }
+    
+    public String GetBuilding( BuildingTypes type, int level){
+        if (level > 3 || level < 1)
+        {
+            return buildings[type][1];
+        }
+        else
+        {
+            return buildings[type][level];
+        }
+    }
+
+    public Dictionary<BuildingTypes, Dictionary<int, String>> getDictionary()
+    {
+        return buildings;
+    }
+    
+}
