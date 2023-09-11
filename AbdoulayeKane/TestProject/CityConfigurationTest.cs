ec48b3baf44ca7500f29f011909ada4e256dfb1c
Abdoulaye Kane <abdoulaye.kane@studio.unibo.it>
2023-09-11 15:36:45 +0200

    add the settings for the test project and the test class

---
+++ AbdoulayeKane/TestProject/CityConfigurationTest.cs
@@ -0,0 +1,23 @@
+﻿using AbdoulayeKane;
+namespace TestProject;
+
+[TestFixture]
+public class CityConfigurationTest
+{
+    CityConfiguration cityConfiguration = new CityConfiguration();
+    
+    [Test]
+    public void GetHeight()
+    {
+        
+        int height = cityConfiguration.GetHeight();
+        Assert.IsTrue(height == 5);
+    }
+
+    [Test]
+    public void GetWidth()
+    {
+        int width = cityConfiguration.GetHeight();
+        Assert.IsTrue(width == 5);
+    }
+}
---
+++ AbdoulayeKane/TestProject/PathIconsConfigurationTest.cs
@@ -0,0 +1,29 @@
+﻿using AbdoulayeKane;
+namespace TestProject;
+
+[TestFixture]
+public class PathIconsConfigurationTest
+{
+
+        public PathIconsConfiguration PathIconsConfiguration = new PathIconsConfiguration();
+        
+        [Test]
+        public void CheckKey()
+        {
+                Dictionary<PathIconsConfiguration.BuildingTypes, Dictionary<int, String>> buildings =
+                        PathIconsConfiguration.getDictionary();
+                Assert.IsTrue(buildings.ContainsKey(PathIconsConfiguration.BuildingTypes.FARM));
+                Assert.IsNotEmpty(buildings); 
+        }
+        
+        [Test]
+        public void GetBuildings()
+        {
+                Dictionary<PathIconsConfiguration.BuildingTypes, Dictionary<int, String>> buildings =
+                        PathIconsConfiguration.getDictionary();
+                Assert.AreEqual(PathIconsConfiguration.GetBuilding(PathIconsConfiguration.BuildingTypes.HALL, 5),
+                        buildings[PathIconsConfiguration.BuildingTypes.HALL][1]);
+                Assert.AreEqual(PathIconsConfiguration.GetBuilding(PathIconsConfiguration.BuildingTypes.LUMBERJACK, 1),
+                        buildings[PathIconsConfiguration.BuildingTypes.LUMBERJACK][1]);
+        }
+}
